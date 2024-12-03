d5491bed16a05eaf7c90f3e9dd8b68f3d9995aef
Mohamed Triki <123988516+MrMohamedtriki@users.noreply.github.com>
2024-12-03 17:19:11 +0100

    Initial commit for Spring Boot backend

---
+++ src/main/java/com/project/gestion_service_clientele/GestionServiceClienteleApplication.java
@@ -0,0 +1,13 @@
+package com.project.gestion_service_clientele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionServiceClienteleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionServiceClienteleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gestion_service_clientele/GestionServiceClienteleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestion_service_clientele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionServiceClienteleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
